7a58896f089d60318336b4829c85d8de821d8fd5
Ivan <maramorosh2@gmail.com>
2026-03-01 16:35:20 -0500

    Added getters and setters to Req and ReqItems Models

--- manager/src/main/java/com/constructionmanager/manager/model/RequisitionContractItems.java
+++ manager/src/main/java/com/constructionmanager/manager/model/RequisitionContractItems.java
@@ -20,4 +20,75 @@ public class RequisitionContractItems {
     private BigDecimal totalCompleted;
     private Integer percentCompleted;
     private BigDecimal totalToFinish;
+
+    @ManyToOne
+    @JoinColumn(name="requisition_id")
+    private Requisitions requisition;
+
+    public RequisitionContractItems() {}
+
+    public RequisitionContractItems(String name, BigDecimal totalCost, BigDecimal retainage) {
+        this.name = name;
+        this.totalCost = totalCost;
+        this.retainage = retainage;
+        this.previousReq = null;
+        this.thisReq = null;
+        this.presentlyStoredMaterial = null;
+        this.totalCompleted = null;
+        this.percentCompleted = null;
+        this.totalToFinish = null;
+    }
+
+    public RequisitionContractItems(
+       String name,
+       BigDecimal totalCost,
+       BigDecimal retainage,
+       BigDecimal previousReq,
+       BigDecimal thisReq,
+       BigDecimal presentlyStoredMaterial,
+       BigDecimal totalCompleted,
+       Integer percentCompleted,
+       BigDecimal totalToFinish
+    ) {
+        this.name = name;
+        this.totalCost = totalCost;
+        this.retainage = retainage;
+        this.previousReq = previousReq;
+        this.thisReq = thisReq;
+        this.presentlyStoredMaterial = presentlyStoredMaterial;
+        this.totalCompleted = totalCompleted;
+        this.percentCompleted = percentCompleted;
+        this.totalToFinish = totalToFinish;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public BigDecimal getTotalCost() {return totalCost;}
+    public void setTotalCost(BigDecimal totalCost) {this.totalCost = totalCost;}
+
+    public BigDecimal getRetainage() {return retainage;}
+    public void setRetainage(BigDecimal retainage) {this.retainage = retainage;}
+
+    public BigDecimal getPreviousReq() {return previousReq;}
+    public void setPreviousReq(BigDecimal previousReq) {this.previousReq = previousReq;}
+
+    public BigDecimal getThisReq() {return thisReq;}
+    public void setThisReq(BigDecimal thisReq) {this.thisReq = thisReq;}
+
+    public BigDecimal getPresentlyStoredMaterial() {return presentlyStoredMaterial;}
+    public void setPresentlyStoredMaterial(BigDecimal presentlyStoredMaterial) {
+        this.presentlyStoredMaterial = presentlyStoredMaterial;
+    }
+
+    public BigDecimal getTotalCompleted() {return totalCompleted;}
+    public void setTotalCompleted(BigDecimal totalCompleted) {this.totalCompleted = totalCompleted;}
+
+    public Integer getPercentCompleted() {return percentCompleted;}
+    public void setPercentCompleted(Integer percentCompleted) {this.percentCompleted = percentCompleted;}
+
+    public BigDecimal getTotalToFinish() {return totalToFinish;}
+    public void setTotalToFinish(BigDecimal totalToFinish) {
+        this.totalToFinish = totalToFinish;
+    }
 }
--- manager/src/main/java/com/constructionmanager/manager/model/Requisitions.java
+++ manager/src/main/java/com/constructionmanager/manager/model/Requisitions.java
@@ -25,7 +25,7 @@ public class Requisitions {
     * 8. Base Contract items like Mobilization, Domestic Water piping,
     *       Sanitary Piping... including breakdown price (Schedule Value(
     *       price to complete item per job), Total completed, Balance to finish
-    *       Retainage Value(10%)) -
+    *       Retainage Value(10%)) +
     * 9. Contingencies & Allowance -
     * 10. Change Orders -
     * 11. Grand Totals - SUM with all CO's, previous req billed $,
@@ -39,9 +39,51 @@ public class Requisitions {
     private BigDecimal totalMoneyReceived;
     private Integer retainage;
     private String companyName;
-    private String OwnerOrRepresentativeFullName;
+    private String ownerOrRepresentativeFullName;
 //    This will be relationship between One this table and Many other table with each element of oter like (Mobilization, Domestic water...)
     @OneToMany(mappedBy = "requisitions", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<RequisitionContractItems> requisitionContractItems;
+
+    public Requisitions() {}
+
+    public Requisitions(
+            BigDecimal contractPrice,
+            BigDecimal totalChangeOrderAmount,
+            BigDecimal totalContractAndCOAmount,
+            BigDecimal totalMoneyReceived,
+            Integer retainage,
+            String companyName,
+            String ownerOrRepresentativeFullName,
+            List<RequisitionContractItems> requisitionContractItems) {
+        this.contractPrice = contractPrice;
+        this.totalChangeOrderAmount = totalChangeOrderAmount;
+        this.totalContractAndCOAmount = totalContractAndCOAmount;
+        this.totalMoneyReceived = totalMoneyReceived;
+        this.retainage = retainage;
+        this.companyName = companyName;
+        this.ownerOrRepresentativeFullName = ownerOrRepresentativeFullName;
+        this.requisitionContractItems = requisitionContractItems;
+    }
+
+    public BigDecimal getContractPrice() {return contractPrice;}
+    public void setContractPrice(BigDecimal contractPrice) {this.contractPrice = contractPrice;}
+
+    public BigDecimal getTotalChangeOrderAmount() {return totalChangeOrderAmount;}
+    public void setTotalChangeOrderAmount(BigDecimal totalChangeOrderAmount) {this.totalChangeOrderAmount = totalChangeOrderAmount;}
+
+    public BigDecimal getTotalContractAndCOAmount() {return totalContractAndCOAmount;}
+    public void setTotalContractAndCOAmount(BigDecimal totalContractAndCOAmount) {this.totalContractAndCOAmount = totalContractAndCOAmount;}
+
+    public BigDecimal getTotalMoneyReceived() {return totalMoneyReceived;}
+    public void setTotalMoneyReceived(BigDecimal totalMoneyReceived) {this.totalMoneyReceived = totalMoneyReceived;}
+
+    public Integer getRetainage() {return retainage;}
+    public void setRetainage(Integer retainage) {this.retainage = retainage;}
+
+    public String getCompanyName() {return companyName;}
+    public void setCompanyName(String companyName) {this.companyName = companyName;}
+
+    public String getOwnerOrRepresentativeFullName() {return ownerOrRepresentativeFullName;}
+    public void setOwnerOrRepresentativeFullName(String ownerOrRepresentativeFullName) {this.ownerOrRepresentativeFullName = ownerOrRepresentativeFullName;}
 
 }
