9e247aded4c48815a4424f39b15ed9ee211b1cd9
Ivan <maramorosh2@gmail.com>
2026-03-07 17:37:18 -0500

    Start making UI

--- manager/src/main/java/com/constructionmanager/manager/ui/MainApp.java
+++ manager/src/main/java/com/constructionmanager/manager/ui/MainApp.java
@@ -4,7 +4,10 @@ import com.constructionmanager.manager.ManagerApplication;
 import com.constructionmanager.manager.model.Projects;
 import com.constructionmanager.manager.service.ProjectService;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -54,69 +57,19 @@ public class MainApp extends Application {
 //        return projects;
 //    }
 
-    @Override
-    public void start(Stage stage) {
+    private void go() {
+        Stage stage = new Stage();
+        Group group = new Group();
+        Scene scene = new Scene(group, 1280, 720, Color.DARKGRAY);
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 1920, 1080, Color.DARKGRAY);
-
-        Text text = new Text();
-        text.setText("This is welcome message");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-        text.setFill(Color.WHITE);
-
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(500);
-        line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.YELLOWGREEN);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(100);
-        rectangle.setY(100);
-        rectangle.setWidth(100);
-        rectangle.setHeight(100);
-        rectangle.setFill(Color.BLUE);
-        rectangle.setStrokeWidth(5);
-        rectangle.setStroke(Color.WHITE);
-
-        Polygon triangle = new Polygon();
-        triangle.getPoints().setAll(
-                200.0, 200.0,
-                300.0, 300.0,
-                200.0, 300.0);
-        triangle.setFill(Color.YELLOW);
-
-        Circle circle = new Circle();
-        circle.setCenterX(350);
-        circle.setCenterY(350);
-        circle.setRadius(50);
-        circle.setFill(Color.ORANGE);
-
-        Image projectIcon = new Image(getClass().getResource("/icons/Projects-icon.png").toExternalForm());
-        ImageView imageView = new ImageView(projectIcon);
-        imageView.setX(400);
-        imageView.setY(400);
-
-
-
-        Image logo = new Image(getClass().getResource("/icons/Maramorosh-Digital-Logo-removebg.png").toExternalForm());
-        stage.getIcons().add(logo);
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(rectangle);
-        root.getChildren().add(triangle);
-        root.getChildren().add(circle);
-        root.getChildren().add(imageView);
-
-        stage.setTitle("Construction Manage");
         stage.setScene(scene);
         stage.show();
+
+    }
+
+    @Override
+    public void start(Stage stage) {
+        go();
     }
 
     public static void main(String[] args) {
---
+++ manager/src/main/java/com/constructionmanager/manager/ui/view/DashboardView.java
@@ -0,0 +1,4 @@
+package com.constructionmanager.manager.ui.view;
+
+public class DashboardView {
+}
